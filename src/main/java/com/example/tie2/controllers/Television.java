5c55f5c942fbd3067c53998bf25eabbbd3caef61
LuciaRyan <lcryan06@gmail.com>
2023-08-29 11:42:05 +0200

    added Television class proforma

--- src/main/java/com/example/tie2/controllers/Television.java
+++ src/main/java/com/example/tie2/controllers/Television.java
@@ -1,4 +1,9 @@
 package com.example.tie2.controllers;
 
 public class Television {
+
+    private String television;
+    public String findById(Long televisionId) {
+        return television;
+    }
 }
