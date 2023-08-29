be6a1b082eb61b0d3563b2fb533793105fd193b1
LuciaRyan <lcryan06@gmail.com>
2023-08-29 13:31:53 +0200

    added example code for a books related application - just a general code from a codejava.net

--- src/main/java/com/example/tie2/controllers/Television.java
+++ src/main/java/com/example/tie2/controllers/Television.java
@@ -1,5 +1,7 @@
 package com.example.tie2.controllers;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 public class Television {
     /*  private final String television;*/
     private final String televisionName;
@@ -27,3 +29,48 @@ public class Television {
         return Television;
     }
 }*/ //TODO: make a working getById method //
+
+
+/*
+@Entity
+@Table(name = “books”)
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String isbn;
+    @Column(name = "title")
+    private String mainTitle;
+    private String genre;
+    Public Long getId(){
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+    Public void setId(Long id){
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+} */
