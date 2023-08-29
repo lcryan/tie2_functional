2793508c1845987cdfb1e13b233190684c1d4328
LuciaRyan <lcryan06@gmail.com>
2023-08-29 11:57:19 +0200

    made getters for attributes - not quite sure about the getById here - has to be clarified

--- src/main/java/com/example/tie2/controllers/Television.java
+++ src/main/java/com/example/tie2/controllers/Television.java
@@ -1,9 +1,29 @@
 package com.example.tie2.controllers;
 
 public class Television {
+    /*  private final String television;*/
+    private final String televisionName;
+    private final long id;
 
-    private String television;
-    public String findById(Long televisionId) {
-        return television;
+
+    public String getTelevisionName() {
+        return televisionName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Television(String televisionName, long id) {
+
+
+        this.televisionName = televisionName;
+        this.id = id;
+
     }
 }
+
+  /*  public String findById(Long televisionId) {
+        return Television;
+    }
+}*/ //TODO: make a working getById method //
