7d51665fe27539076ede3f5cbe3d5c69edf3313c
LuciaRyan <lcryan06@gmail.com>
2023-08-29 13:29:27 +0200

    added code to RecordNotFoundException - standard code from EdHub under 3.4

--- src/main/java/com/example/tie2/exceptions/RecordNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.tie2.exceptions;
-
-public class RecordNotFound {
-}
-
-
-// TODO: extend this class with RunTimeException //
---
+++ src/main/java/com/example/tie2/exceptions/RecordNotFoundException.java
@@ -0,0 +1,18 @@
+package com.example.tie2.exceptions;
+
+import java.io.Serial;
+
+public class RecordNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException() {
+        super();
+    }
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
