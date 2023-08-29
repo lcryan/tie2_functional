50f5514daba0570132b49682d6a2761724ccbf4f
LuciaRyan <lcryan06@gmail.com>
2023-08-29 11:58:24 +0200

    pushing through Television television as a parameter in the @methods - not quite sure, if this will be functional in the end

--- src/main/java/com/example/tie2/controllers/TelevisionController.java
+++ src/main/java/com/example/tie2/controllers/TelevisionController.java
@@ -8,23 +8,22 @@ import java.util.List;
 @RestController
 public class TelevisionController {
 
-//TODO: need to make a database for the retrieval of televisions;//
+    //TODO: need to make a database for the retrieval of televisions;//
     @GetMapping("/television/{id}")
-    public ResponseEntity<Television> getTelevisionById(@PathVariable(value = "id") Long televisionId, Television television) {
-
-        Television television1 = television;
+    public ResponseEntity<Television> getTelevisionById(@PathVariable(value = "id") Long id, Television television) {
+        // TODO: need to build a method within class Television to retrieve televisionById - could be achieved with ById method? //
         return ResponseEntity.ok(television); // the ResponseEntity.ok(television) call creates a response with an HTTP status of 200 OK and includes television as the response body
     }
 
-    @GetMapping("/getAllTelevisions") // same question here - do we need id ?? //
+    @GetMapping("/allTelevisions") // same question here - do we need id ?? //
     public ResponseEntity<List<String>> getAllTelevisions(List<String> allTelevisions) {
         return ResponseEntity.ok(allTelevisions);
 
     }
 
     @PostMapping("/postTelevision")
-    public ResponseEntity<Object> postTelevision() {
-        String television = "Samsung BB6547";
+    public ResponseEntity<Object> postTelevision(Television television) {
+        television = new Television("Samsung 3", 90762639);
         return ResponseEntity.created(null).body(television);
     }
 
@@ -34,16 +33,15 @@ public class TelevisionController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<String> putTelevision(@PathVariable long id, @RequestBody String updatedTelevision) {
-        // how to retrieve the actual television here ??? //
+    public ResponseEntity<String> putTelevision(@PathVariable long id, @RequestBody Television television) {
+        // how to retrieve the actual television here ??? // //TODO: probably need a database to retrieve this from. //
 
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteTelevision(@PathVariable long id, @RequestBody String deletedTelevision) {
+    public ResponseEntity<Object> deleteTelevision(@PathVariable long id, @RequestBody Television television) {
         return ResponseEntity.noContent().build();
     }
 }
 
-// notes: maybe better to change updated and deletedTelevision into general television ?? //
