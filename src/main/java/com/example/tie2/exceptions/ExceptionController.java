68d17e95831343c52b31d995e1bc1565173cd5ad
LuciaRyan <lcryan06@gmail.com>
2023-08-29 13:13:18 +0200

    made exception controller in exceptions folder

---
+++ src/main/java/com/example/tie2/exceptions/ExceptionController.java
@@ -0,0 +1,7 @@
+package com.example.tie2.exceptions;
+
+public class ExceptionController {
+}
+
+
+//TODO: 1. write the according annotations for the ExceptionController. 2. Make an exceptionHandler within the ExceptionController class with annotations and response entity. //
