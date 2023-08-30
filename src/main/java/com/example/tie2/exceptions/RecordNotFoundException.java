7e35a2bed90a4b16fc33247902a85c9770a1b2e7
LuciaRyan <lcryan06@gmail.com>
2023-08-30 10:57:53 +0200

    added explanatory comment about RunTimeException

--- src/main/java/com/example/tie2/exceptions/RecordNotFoundException.java
+++ src/main/java/com/example/tie2/exceptions/RecordNotFoundException.java
@@ -1,10 +1,10 @@
 package com.example.tie2.exceptions;
 
 import java.io.Serial;
-
+// Runtime Exception
 public class RecordNotFoundException extends RuntimeException {
     @Serial
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // can be thrown out //
 
     public RecordNotFoundException() {
         super();
