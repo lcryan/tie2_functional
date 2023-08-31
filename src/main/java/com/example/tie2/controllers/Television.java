20728226048582ff7d6f4e8c325c5be71fe3f19d
LuciaRyan <lcryan06@gmail.com>
2023-08-31 11:00:25 +0200

    added more attributes and according constructors/getters/setters - boolean and int

--- src/main/java/com/example/tie2/controllers/Television.java
+++ src/main/java/com/example/tie2/controllers/Television.java
@@ -1,7 +1,34 @@
 package com.example.tie2.controllers;
 
 public class Television {
-    String televisionName;
+    private String televisionName;
+
+
+    public int getTotalStock() {
+        return totalStock;
+    }
+
+    public void setTotalStock(int totalStock) {
+        this.totalStock = totalStock;
+    }
+
+    private int totalStock;
+    private boolean HD;
+
+
+    public Television(int totalStock) {
+        this.totalStock = totalStock;
+    }
+
+
+    public boolean isHD() {
+        return HD;
+    }
+
+    public void setHD(boolean HD) {
+        this.HD = HD;
+    }
+
 
     public void setTelevisionName(String televisionName) {
         this.televisionName = televisionName;
@@ -13,8 +40,10 @@ public class Television {
     }
 
 
-    public Television(String televisionName) {
+    public Television(String televisionName, boolean HD, int totalStock) {
         this.televisionName = televisionName;
+        this.HD = HD;
+        this.totalStock = totalStock;
 
     }
 }
