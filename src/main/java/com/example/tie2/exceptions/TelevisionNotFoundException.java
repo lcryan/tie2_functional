abb3887593d49e503f75799fe5d0da1f1db184bc
LuciaRyan <lcryan06@gmail.com>
2023-09-02 18:42:15 +0200

    Changed Name of ExceptionController to TelevisionNotFound instead of RecordNotFound

--- src/main/java/com/example/tie2/exceptions/TelevisionNotFoundException.java
+++ src/main/java/com/example/tie2/exceptions/TelevisionNotFoundException.java
@@ -1,16 +1,17 @@
 package com.example.tie2.exceptions;
 
 import java.io.Serial;
+
 // Runtime Exception
 public class TelevisionNotFoundException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L; // can be thrown out //
 
-    public RecordNotFoundException() {
+    public TelevisionNotFoundException() {
         super();
     }
 
-    public RecordNotFoundException(String message) {
+    public TelevisionNotFoundException(String message) {
         super(message);
     }
 }
