3e7803ac5db8dd36eb28371b0488ae121d71640e
LuciaRyan <lcryan06@gmail.com>
2023-09-02 19:37:41 +0200

    made a new TelevisionNotFoundException - as the other one was not functional - functional now though

--- src/main/java/com/example/tie2/exceptions/TelevisionNotFoundException.java
+++ src/main/java/com/example/tie2/exceptions/TelevisionNotFoundException.java
@@ -1,19 +1,12 @@
 package com.example.tie2.exceptions;
 
-import java.io.Serial;
-
-// Runtime Exception
 public class TelevisionNotFoundException extends RuntimeException {
-    @Serial
-    private static final long serialVersionUID = 1L; // can be thrown out //
-
-    public TelevisionNotFoundException() {
-        super();
-    }
 
     public TelevisionNotFoundException(String message) {
         super(message);
     }
-}
 
-
+    public TelevisionNotFoundException() {
+        super();
+    }
+} // this here doesn't return anything //
