b6985ba0b853c5bd99d511c26e28a8b2bbfb6099
LuciaRyan <lcryan06@gmail.com>
2023-09-07 10:51:26 +0200

    extended repo with jpa and added generics Televisiona, Long

--- src/main/java/com/example/tie2/repositories/TelevisionRepository.java
+++ src/main/java/com/example/tie2/repositories/TelevisionRepository.java
@@ -1,4 +1,7 @@
 package com.example.tie2.repositories;
 
-public interface TelevisionRepository {
+import com.example.tie2.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
 }
