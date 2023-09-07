e8210d47702794e4a3331d7d1a15fdf36d467291
LuciaRyan <lcryan06@gmail.com>
2023-09-07 11:51:46 +0200

    deletion of all @mappings methods complete

--- src/main/java/com/example/tie2/controllers/TelevisionController.java
+++ src/main/java/com/example/tie2/controllers/TelevisionController.java
@@ -21,31 +21,9 @@ public class TelevisionController {
     }
 
 
-    @GetMapping("/televisions/{id}") //gets back one single television // functional check //
-    public ResponseEntity<Television> getOneTelevision(@PathVariable Long id) {
-        Optional<Television> television = televisionRepository.findById(id);
-        if (id >= 0 && id < televisions.size()) {
-            Television getByIdTelevision = televisions.get(id);
-            return new ResponseEntity<>(getByIdTelevision, HttpStatus.OK);
-        } else {
-            throw new TelevisionNotFoundException("The requested object television cannot be found."); //prints this message, if television not found//
-            /*return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);*/  //uncommented - to check, if throw Exception works //
-        }
-    }
-
-    @GetMapping("/televisions") //gets back all televisions of the database// //functional - check //
-    public ResponseEntity<List<Television>> getAllTelevisions(@RequestParam(value = "brand", required = false) String brand) {
-        List<Television> televisions;
-
-        if (brand == null) {
-            televisions = televisionRepository.findAll();
-        }
-    } else
-
-    {
 
     }
-}
+
 
 
 
