da33309afd3c15e49cd8927b4fba28274a5d05e6
LuciaRyan <lcryan06@gmail.com>
2023-09-13 11:39:19 +0200

    set up TelevisionDTO

--- src/main/java/com/example/tie2/dtos/TelevisionDto.java
+++ src/main/java/com/example/tie2/dtos/TelevisionDto.java
@@ -1,2 +1,209 @@
-package com.example.tie2.dtos;public class TelevisionDto {
+package com.example.tie2.dtos;
+
+import com.example.tie2.models.EnergyLabel;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TelevisionDto {
+
+    private Long id;
+
+    private String type;
+
+    private String brand;
+
+    private String name;
+
+    private double price;
+
+    private double availableSize;
+
+    private double refreshRate;
+
+    private String screenType;
+
+    private String screenQuality;
+
+    private boolean smartTv;
+
+    private boolean wifi;
+
+    private boolean voiceControl;
+
+    private boolean hdr;
+
+    private boolean bluetooth;
+
+    private boolean ambiLight;
+
+    private Integer originalStock;
+
+    private Integer sold;
+
+    private LocalDate dateOfPurchase;
+
+    private LocalDateTime currentStock;
+
+    private EnergyLabel energyLabel; // pulling from enum class //
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getAvailableSize() {
+        return availableSize;
+    }
+
+    public void setAvailableSize(double availableSize) {
+        this.availableSize = availableSize;
+    }
+
+    public double getRefreshRate() {
+        return refreshRate;
+    }
+
+    public void setRefreshRate(double refreshRate) {
+        this.refreshRate = refreshRate;
+    }
+
+    public String getScreenType() {
+        return screenType;
+    }
+
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
+
+    public String getScreenQuality() {
+        return screenQuality;
+    }
+
+    public void setScreenQuality(String screenQuality) {
+        this.screenQuality = screenQuality;
+    }
+
+    public boolean isSmartTv() {
+        return smartTv;
+    }
+
+    public void setSmartTv(boolean smartTv) {
+        this.smartTv = smartTv;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public boolean isVoiceControl() {
+        return voiceControl;
+    }
+
+    public void setVoiceControl(boolean voiceControl) {
+        this.voiceControl = voiceControl;
+    }
+
+    public boolean isHdr() {
+        return hdr;
+    }
+
+    public void setHdr(boolean hdr) {
+        this.hdr = hdr;
+    }
+
+    public boolean isBluetooth() {
+        return bluetooth;
+    }
+
+    public void setBluetooth(boolean bluetooth) {
+        this.bluetooth = bluetooth;
+    }
+
+    public boolean isAmbiLight() {
+        return ambiLight;
+    }
+
+    public void setAmbiLight(boolean ambiLight) {
+        this.ambiLight = ambiLight;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public Integer getSold() {
+        return sold;
+    }
+
+    public void setSold(Integer sold) {
+        this.sold = sold;
+    }
+
+    public LocalDate getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(LocalDate dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public LocalDateTime getCurrentStock() {
+        return currentStock;
+    }
+
+    public void setCurrentStock(LocalDateTime currentStock) {
+        this.currentStock = currentStock;
+    }
+
+    public EnergyLabel getEnergyLabel() {
+        return energyLabel;
+    }
+
+    public void setEnergyLabel(EnergyLabel energyLabel) {
+        this.energyLabel = energyLabel;
+    }
 }
