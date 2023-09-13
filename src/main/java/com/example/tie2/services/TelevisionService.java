8cd7c3433923f935f22ad635935528bcdd51ce2d
LuciaRyan <lcryan06@gmail.com>
2023-09-13 11:40:52 +0200

    service not complete yet - still have to set all attributes and get

--- src/main/java/com/example/tie2/services/TelevisionService.java
+++ src/main/java/com/example/tie2/services/TelevisionService.java
@@ -1,2 +1,30 @@
-package com.example.tie2.services;public class TelevisionService {
+package com.example.tie2.services;
+
+import com.example.tie2.dtos.TelevisionDto;
+import com.example.tie2.models.Television;
+import com.example.tie2.repositories.TelevisionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TelevisionService {
+
+    private final TelevisionRepository televisionRepository;
+
+    public TelevisionService(TelevisionRepository televisionRepository) {
+        this.televisionRepository = televisionRepository;
+    }
+
+    public List<TelevisionDto> getAllTelevisions() {
+        List<Television> televisions = televisionRepository.findAll();
+        List<TelevisionDto> televisionDtos = new ArrayList<>(); // altijd meervoud !! //
+        for (Television television : televisions) {
+            TelevisionDto televisionDto = new TelevisionDto();
+            televisionDto.setBrand(television.getBrand());
+            //TODO: finish stuff here //
+        }
+        return televisionDtos;
+    }
 }
