1a238346cce15c694ffae01974be0b36d9c7a764
LuciaRyan <lcryan06@gmail.com>
2023-09-14 10:01:38 +0200

    added all attribute calls

--- src/main/java/com/example/tie2/services/TelevisionService.java
+++ src/main/java/com/example/tie2/services/TelevisionService.java
@@ -22,15 +22,33 @@ public class TelevisionService {
         List<TelevisionDto> televisionDtos = new ArrayList<>(); // watch out: in case you have a list, don't forget to use plural - here: televisions//
         for (Television television : televisions) {
             TelevisionDto televisionDto = new TelevisionDto();
+            televisionDto.setId(television.getId());
             televisionDto.setBrand(television.getBrand());
-            // TODO : add all attributes here //
+            televisionDto.setName(television.getName());
+            televisionDto.setPrice(television.getPrice());
+            televisionDto.setAvailableSize(television.getAvailableSize());
+            televisionDto.setScreenQuality(television.getScreenQuality());
+            televisionDto.setSmartTv(television.isSmartTv());
+            televisionDto.setWifi(television.isWifi());
+            televisionDto.setVoiceControl(television.isVoiceControl());
+            televisionDto.setHdr(television.isHdr());
+            televisionDto.setBluetooth(television.isBluetooth());
+            televisionDto.setAmbiLight(television.isAmbiLight());
+            televisionDto.setOriginalStock(television.getOriginalStock());
+            televisionDto.setOriginalStock(television.getOriginalStock());
+            televisionDto.setSold(television.getSold());
+            televisionDto.setDateOfPurchase(television.getDateOfPurchase());
+            televisionDto.setCurrentStock(television.getCurrentStock());
+            televisionDto.setEnergyLabel(television.getEnergyLabel());
         }
         return televisionDtos;
     }
 }
 
 
-// TODO here: 1. make function to get all televisions;
+// TODO here:
+//  1A.finish getAllTelevisions!
+//  1. make function to get all televisions;
 //  2. a function to get one television;
 //  3. a function to post one television;
 //  4. a function to delete one television;
