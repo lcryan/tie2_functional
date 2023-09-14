92953b519b4a77024b805c08e36071aad3e5e4e6
LuciaRyan <lcryan06@gmail.com>
2023-09-14 11:45:49 +0200

    added helper methods for transfer Television to TelevisionDto and TelevisionDtoToTelevsion

--- src/main/java/com/example/tie2/services/TelevisionService.java
+++ src/main/java/com/example/tie2/services/TelevisionService.java
@@ -23,45 +23,73 @@ public class TelevisionService {
         List<Television> televisions = televisionRepository.findAll();
         List<TelevisionDto> televisionDtos = new ArrayList<>(); // watch out: in case you have a list, don't forget to use plural - here: televisions//
         for (Television television : televisions) {
-            TelevisionDto televisionDto = new TelevisionDto();
-            televisionDto.setId(television.getId());
-            televisionDto.setBrand(television.getBrand());
-            televisionDto.setName(television.getName());
-            televisionDto.setPrice(television.getPrice());
-            televisionDto.setAvailableSize(television.getAvailableSize());
-            televisionDto.setScreenQuality(television.getScreenQuality());
-            televisionDto.setSmartTv(television.isSmartTv());
-            televisionDto.setWifi(television.isWifi());
-            televisionDto.setVoiceControl(television.isVoiceControl());
-            televisionDto.setHdr(television.isHdr());
-            televisionDto.setBluetooth(television.isBluetooth());
-            televisionDto.setAmbiLight(television.isAmbiLight());
-            televisionDto.setOriginalStock(television.getOriginalStock());
-            televisionDto.setOriginalStock(television.getOriginalStock());
-            televisionDto.setSold(television.getSold());
-            televisionDto.setDateOfPurchase(television.getDateOfPurchase());
-            televisionDto.setCurrentStock(television.getCurrentStock());
-            televisionDto.setEnergyLabel(television.getEnergyLabel());
+            TelevisionDto televisionDto = transferTelevisionToTelevisionDto(television);
+            televisionDtos.add(televisionDto);
         }
         return televisionDtos;
     }
 
     public TelevisionDto getOneTelevision(Long id) {
-        Optional<Television> optionalTelevision =televisionRepository.findById(id);
+        Optional<Television> optionalTelevision = televisionRepository.findById(id);
         if (optionalTelevision.isPresent()) {
             Television televisionDto = optionalTelevision.get();
-            return new TelevisionDto(televisionDto);
 
 
         } else {
             throw new TelevisionNotFoundException("No television found with the following id " + id + ".");
         }
     }
+// ******* helper functions ******* //
 
+    private TelevisionDto transferTelevisionToTelevisionDto(Television television) {
+        TelevisionDto televisionDto = new TelevisionDto();
+        televisionDto.setId(television.getId());
+        televisionDto.setBrand(television.getBrand());
+        televisionDto.setName(television.getName());
+        televisionDto.setPrice(television.getPrice());
+        televisionDto.setAvailableSize(television.getAvailableSize());
+        televisionDto.setScreenQuality(television.getScreenQuality());
+        televisionDto.setSmartTv(television.isSmartTv());
+        televisionDto.setWifi(television.isWifi());
+        televisionDto.setVoiceControl(television.isVoiceControl());
+        televisionDto.setHdr(television.isHdr());
+        televisionDto.setBluetooth(television.isBluetooth());
+        televisionDto.setAmbiLight(television.isAmbiLight());
+        televisionDto.setOriginalStock(television.getOriginalStock());
+        televisionDto.setSold(television.getSold());
+        televisionDto.setDateOfPurchase(television.getDateOfPurchase());
+        televisionDto.setCurrentStock(television.getCurrentStock());
+        televisionDto.setEnergyLabel(television.getEnergyLabel());
+        televisionDto.setTelevision(television.getTelevision());
+        return televisionDto;
+    }
+
+    private Television transferTelevisionDtoToTelevision(TelevisionDto televisionDto) {
+        Television television = new Television();
+        television.setId(televisionDto.getId());
+        television.setBrand(televisionDto.getBrand());
+        television.setName(televisionDto.getName());
+        television.setPrice(televisionDto.getPrice());
+        television.setAvailableSize(televisionDto.getAvailableSize());
+        television.setScreenQuality(televisionDto.getScreenQuality());
+        television.setSmartTv(televisionDto.isSmartTv());
+        television.setWifi(televisionDto.isWifi());
+        television.setVoiceControl(televisionDto.isVoiceControl());
+        television.setHdr(televisionDto.isHdr());
+        television.setBluetooth(televisionDto.isBluetooth());
+        television.setAmbiLight(televisionDto.isAmbiLight());
+        television.setOriginalStock(televisionDto.getOriginalStock());
+        television.setSold(televisionDto.getSold());
+        television.setDateOfPurchase(televisionDto.getDateOfPurchase());
+        television.setCurrentStock(televisionDto.getCurrentStock());
+        television.setEnergyLabel(televisionDto.getEnergyLabel());
+        television.setTelevision(televisionDto.getTelevision());
+        return television;
+    }
 }
 
 
-// TODO here:
+// TODO here: // 1. make helper functions for TelevisionDto to Television model - and the other way around //
 //  2. a function to get one television;
 //  3. a function to post one television;
 //  4. a function to delete one television;
