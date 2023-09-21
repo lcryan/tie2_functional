73afd16163264e77c836b6820e29709dba00f4cf
LuciaRyan <lcryan06@gmail.com>
2023-09-21 17:19:29 +0200

    remote control model finalized

--- src/main/java/com/example/tie2/models/RemoteControl.java
+++ src/main/java/com/example/tie2/models/RemoteControl.java
@@ -1,4 +1,97 @@
 package com.example.tie2.models;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table
 public class RemoteControl {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "brand")
+    private String brand;
+    @Column(name = "compatible_with")
+    private String compatibleWith;
+    @Column(name = "battery_type")
+    private String batteryType;
+    @Column(name = "original_stock")
+    private Integer originalStock;
+    @Column(name = "price")
+    private double price;
+
+    public RemoteControl(Long id, String name, String brand, String compatibleWith, String batteryType, Integer originalStock, double price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.compatibleWith = compatibleWith;
+        this.batteryType = batteryType;
+        this.originalStock = originalStock;
+        this.price = price;
+    }
+
+    public RemoteControl() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCompatibleWith() {
+        return compatibleWith;
+    }
+
+    public void setCompatibleWith(String compatibleWith) {
+        this.compatibleWith = compatibleWith;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
