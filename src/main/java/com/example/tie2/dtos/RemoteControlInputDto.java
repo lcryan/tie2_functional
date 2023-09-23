c2afeeea1de09a3fb3a5b998f5006d30d910e33e
LuciaRyan <lcryan06@gmail.com>
2023-09-23 10:27:21 +0200

    added RemoteControl Input Dto - assigned all validation fields necessary too

---
+++ src/main/java/com/example/tie2/dtos/RemoteControlInputDto.java
@@ -0,0 +1,73 @@
+package com.example.tie2.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+public class RemoteControlInputDto {
+    private Long id;
+    @NotEmpty(message = "Name has to be assigned.")
+    private String name;
+    private String brand;
+    private String compatibleWith;
+    private String batteryType;
+    @Positive(message = "Stock has to be higher than zero.")
+    private Integer originalStock;
+    @Positive(message = "Price has to be assigned a positive number.")
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCompatibleWith() {
+        return compatibleWith;
+    }
+
+    public void setCompatibleWith(String compatibleWith) {
+        this.compatibleWith = compatibleWith;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
