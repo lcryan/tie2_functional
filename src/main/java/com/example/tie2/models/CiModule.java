338f6ed069d94667eb2acaf161cf5f8b4ef96cc3
LuciaRyan <lcryan06@gmail.com>
2023-09-23 09:53:02 +0200

    CiModule model set up

--- src/main/java/com/example/tie2/models/CiModule.java
+++ src/main/java/com/example/tie2/models/CiModule.java
@@ -1,4 +1,49 @@
 package com.example.tie2.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class CiModule {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private double price;
+
+    public CiModule(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public CiModule() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
