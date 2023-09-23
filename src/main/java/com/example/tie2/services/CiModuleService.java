d43691a4755949093d0ddceb5c0f9cd926c3d9fc
LuciaRyan <lcryan06@gmail.com>
2023-09-23 10:48:50 +0200

    added helper methods to transfer from model to dto and inputDto to model

--- src/main/java/com/example/tie2/services/CiModuleService.java
+++ src/main/java/com/example/tie2/services/CiModuleService.java
@@ -1,4 +1,40 @@
 package com.example.tie2.services;
 
+import com.example.tie2.dtos.CiModuleDto;
+import com.example.tie2.dtos.CiModuleInputDto;
+import com.example.tie2.models.CiModule;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CiModuleService {
+
+    private final CiModuleService ciModuleService;
+
+    public CiModuleService(CiModuleService ciModuleService) {
+        this.ciModuleService = ciModuleService;
+    }
+
+    // helper methods for conversion model - dto- input dto //
+
+    public CiModuleDto transferCiModuleToCiModuleDto(CiModule ciModule) {
+        CiModuleDto ciModuleDto = new CiModuleDto();
+        ciModuleDto.setId(ciModule.getId());
+        ciModuleDto.setName(ciModule.getName());
+        ciModuleDto.setType(ciModule.getType());
+        ciModuleDto.setPrice(ciModule.getPrice());
+
+        return ciModuleDto;
+    }
+
+    public CiModule transferCiModuleInputDtoToCiModule(CiModuleInputDto ciModuleInputDto) {
+        var ciModule = new CiModule();
+
+        ciModule.setId(ciModuleInputDto.getId());
+        ciModuleInputDto.setName(ciModuleInputDto.getName());
+        ciModuleInputDto.setType(ciModuleInputDto.getType());
+        ciModuleInputDto.setPrice(ciModuleInputDto.getPrice());
+
+        return ciModule;
+    }
+
 }
