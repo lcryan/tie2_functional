990e0c6be877f705f436c083270a699245b691b3
LuciaRyan <lcryan06@gmail.com>
2023-09-25 11:41:07 +0200

    completed ci Module controller

--- src/main/java/com/example/tie2/controllers/CiModuleController.java
+++ src/main/java/com/example/tie2/controllers/CiModuleController.java
@@ -1,4 +1,51 @@
 package com.example.tie2.controllers;
 
+import com.example.tie2.dtos.CiModuleDto;
+import com.example.tie2.dtos.CiModuleInputDto;
+import com.example.tie2.services.CiModuleService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class CiModuleController {
+
+
+    private final CiModuleService ciModuleService;
+
+    public CiModuleController(CiModuleService ciModuleService) {
+        this.ciModuleService = ciModuleService;
+    }
+
+    @GetMapping("/ciModules")
+    public ResponseEntity<List<CiModuleDto>> getAllCiModules() {
+        return ResponseEntity.ok(ciModuleService.getAllCiModules());
+    }
+
+    @PostMapping("/ciModules")
+    public ResponseEntity<CiModuleDto> createNewCiModule(@Valid @RequestBody CiModuleInputDto ciModuleInputDto) {
+        CiModuleDto ciModuleDto = ciModuleService.createCiModule(ciModuleInputDto);
+        return ResponseEntity.created(null).body(ciModuleDto);
+    }
+
+    @GetMapping("/ciModules/{id}")
+    public ResponseEntity<CiModuleDto> getCiModule(@PathVariable Long id) {
+        return ResponseEntity.ok(ciModuleService.getOneCiModule(id));
+    }
+
+    @DeleteMapping("/ciModules/{id}")
+    public ResponseEntity<Optional<CiModuleDto>> deleteCiModule(@PathVariable Long id) {
+        ciModuleService.deleteCiModule(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("televisions/{id}")
+
+    public ResponseEntity<CiModuleDto> updateCiModule(@PathVariable Long id, @Valid @RequestBody CiModuleInputDto ciModuleInputDto) {
+        CiModuleDto ciModuleDtoOne = ciModuleService.updateCiModule(id, ciModuleInputDto);
+        return ResponseEntity.ok().body(ciModuleDtoOne);
+    }
 }
