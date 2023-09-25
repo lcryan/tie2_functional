0a0524c12f343e1a01588d06ca0917f96e7630cb
LuciaRyan <lcryan06@gmail.com>
2023-09-25 16:31:34 +0200

    added IdinputDto - requested in assignment

---
+++ src/main/java/com/example/tie2/dtos/IdInputDto.java
@@ -0,0 +1,17 @@
+package com.example.tie2.dtos;
+
+public class IdInputDto {
+
+    public Long id;
+
+    public IdInputDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
