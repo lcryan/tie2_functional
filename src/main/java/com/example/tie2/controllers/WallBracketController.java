fa59c2b8602bf6c5f967ef58e8e250922e556bac
LuciaRyan <lcryan06@gmail.com>
2023-09-25 13:54:02 +0200

    completed controller for wallbracket

--- src/main/java/com/example/tie2/controllers/WallBracketController.java
+++ src/main/java/com/example/tie2/controllers/WallBracketController.java
@@ -1,4 +1,49 @@
 package com.example.tie2.controllers;
 
+import com.example.tie2.dtos.WallBracketDto;
+import com.example.tie2.dtos.WallBracketInputDto;
+import com.example.tie2.services.WallBracketService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
 public class WallBracketController {
+
+    private final WallBracketService wallBracketService;
+
+    public WallBracketController(WallBracketService wallBracketService) {
+        this.wallBracketService = wallBracketService;
+    }
+
+    @GetMapping("/wallbrackets")
+    public ResponseEntity<List<WallBracketDto>> getAllWallBrackets() {
+        return ResponseEntity.ok(wallBracketService.getAllWallBrackets());
+    }
+
+    @PostMapping("/wallbrackets")
+    public ResponseEntity<WallBracketDto> createWallBracket(@Valid @RequestBody WallBracketInputDto wallBracketInputDto) {
+        WallBracketDto wallBracketDto = wallBracketService.createWallBracket(wallBracketInputDto);
+        return ResponseEntity.created(null).body(wallBracketDto);
+    }
+
+    @GetMapping("/wallbrackets/{id}")
+    public ResponseEntity<WallBracketDto> getWallBracket(@PathVariable Long id) {
+        return ResponseEntity.ok(wallBracketService.getOneWallBracket(id));
+    }
+
+    @DeleteMapping("/wallbrackets/{id}")
+    public ResponseEntity<Optional<WallBracketDto>> deleteOneWallBracket(@PathVariable Long id) {
+        wallBracketService.deleteWallBracket(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/wallbrackets/{id}")
+    public ResponseEntity<WallBracketDto> updateWallBracket(@PathVariable Long id, @Valid @RequestBody WallBracketInputDto wallBracketInputDto) {
+        WallBracketDto wallBracketDto1 = wallBracketService.updateWallbracket(id, wallBracketInputDto);
+        return ResponseEntity.ok().body(wallBracketDto1);
+    }
 }
