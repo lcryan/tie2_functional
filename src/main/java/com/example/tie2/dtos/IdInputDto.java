861301c5684834238e2fede18975bf174f2946ca
LuciaRyan <lcryan06@gmail.com>
2023-09-26 09:36:40 +0200

    delted unnecessary getter & setter and added @NotNull val

--- src/main/java/com/example/tie2/dtos/IdInputDto.java
+++ src/main/java/com/example/tie2/dtos/IdInputDto.java
@@ -1,14 +1,9 @@
 package com.example.tie2.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 public class IdInputDto {
 
+    @NotNull
     public Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
