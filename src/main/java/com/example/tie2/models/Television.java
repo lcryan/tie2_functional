b8afae4f73a8d2eb0177eac0ad263d4b3f2b7c94
LuciaRyan <lcryan06@gmail.com>
2023-09-29 09:01:07 +0200

    setting up One-To-One relation between Television and Remote Control model and adding comment to the different steps- sort of HOW TO

--- src/main/java/com/example/tie2/models/Television.java
+++ src/main/java/com/example/tie2/models/Television.java
@@ -66,9 +66,11 @@ public class Television {
     @Column(name = "EnergyLabel")
     private EnergyLabel energyLabel; // pulling from enum class //
 
+
+    // A one-to-one relation between Television and Remote Control //
     @OneToOne
     private RemoteControl remoteControl;
-
+// for every television the is ONE remote control //
 
     public Television() {
     }
@@ -233,6 +235,7 @@ public class Television {
         this.sold = sold;
     }
 
+    // getter and setter for the Remote Control - which Television has a one-to-one relation //
     public RemoteControl getRemoteControl() {
         return remoteControl;
     }
