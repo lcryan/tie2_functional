12494bfff5938a40e211143557d29564274e50df
LuciaRyan <lcryan06@gmail.com>
2023-10-02 16:49:45 +0200

    set up sec

--- src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -8,12 +8,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
-    @Bean
+/*    @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
     }
 
     @Bean
     protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
-    }
+    }*/
 }
