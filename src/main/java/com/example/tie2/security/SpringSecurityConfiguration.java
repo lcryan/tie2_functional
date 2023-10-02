497f7374233086285ab006fe556199a31e34de79
LuciaRyan <lcryan06@gmail.com>
2023-10-02 12:56:51 +0200

    added security package and set up SpringSecurityConfig. class

---
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.tie2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    }
+
+    @Bean
+    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
+
+    }
+}
