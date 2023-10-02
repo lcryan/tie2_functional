3bb90c169efbdd1a612ac83a875f913ca84ef601
LuciaRyan <lcryan06@gmail.com>
2023-10-02 17:04:41 +0200

    added pwencoder

--- src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,4 +18,11 @@ public class SpringSecurityConfiguration {
     protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
     }*/
+
+
+    // this sets up the password encoder //
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
