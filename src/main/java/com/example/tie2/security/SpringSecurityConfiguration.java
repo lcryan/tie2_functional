5d103711b3e3fb661624b9888db1721525c9ebad
LuciaRyan <lcryan06@gmail.com>
2023-10-02 17:22:42 +0200

    added code sections - but uncommented

--- src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.tie2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,17 +13,34 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfiguration {
 /*    @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    }
-
-    @Bean
-    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
-
     }*/
 
-
-    // this sets up the password encoder //
+    // sets up pw encoder //
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    // ------ To implement authorisation here ------- -//
+ /*   @Bean
+    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .httpBasic().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/info").hasRole("USER")
+                .requestMatchers("/users/**").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/admins").hasAuthority("ROLE_ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
+                .requestMatchers("/authenticate").permitAll()
+
+                .anyRequest().denyAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }*/
 }
+
+
