112fbe0164ea887f1104719a267f1ebbe4a8cb55
LuciaRyan <lcryan06@gmail.com>
2023-10-03 14:43:06 +0200

    added code to security - has to be reviewed

--- src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+@EnableWebSecurity
 @Configuration
 
 public class SpringSecurityConfiguration {
