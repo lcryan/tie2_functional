c5aed8299709e373fa1f0921fa0d15181d18bedb
LuciaRyan <lcryan06@gmail.com>
2023-10-05 21:11:20 +0200

    set up authresponse

--- src/main/java/com/example/tie2/payload/AuthenticationResponse.java
+++ src/main/java/com/example/tie2/payload/AuthenticationResponse.java
@@ -1,6 +1,14 @@
 package com.example.tie2.payload;
 
 public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
 }
 
-//TODO: has to be like a DTO ? ??
