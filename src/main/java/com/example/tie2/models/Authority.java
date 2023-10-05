65ee8d2287e19479c7cb07a292c9af5944505380
LuciaRyan <lcryan06@gmail.com>
2023-10-05 21:29:40 +0200

    basic set up authority - not quite sure, if this is correct - tbc

--- src/main/java/com/example/tie2/models/Authority.java
+++ src/main/java/com/example/tie2/models/Authority.java
@@ -1,4 +1,44 @@
 package com.example.tie2.models;
 
-public class Authority {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@IdClass(AuthorityKey.class)
+@Table(name = "authorities")
+public class Authority implements Serializable {
+    @Id
+    @Column
+    private String username;
+
+    @Id
+    @Column
+    private String authority;
+
+    public Authority() {
+
+    }
+
+    public Authority(String username, String authority) {
+        this.username = username;
+        this.authority = authority;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
