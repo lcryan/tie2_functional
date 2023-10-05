9b4accd3efa798402245c0188e98a14656fcb643
LuciaRyan <lcryan06@gmail.com>
2023-10-05 21:02:36 +0200

    added very basic set up - no requestMatches build yet - addressing classes first now

--- src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/tie2/security/SpringSecurityConfiguration.java
@@ -1,14 +1,16 @@
-/*
 package com.example.tie2.security;
 
+import com.example.tie2.filter.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,10 +21,13 @@ import javax.sql.DataSource;
 @Configuration
 
 public class SpringSecurityConfiguration {
-    private final DataSource dataSource;
 
-    public SpringSecurityConfiguration(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private UserDetailsService userDetailsService;
+    private JwtRequestFilter jwtRequestFilter;
+
+    public SpringSecurityConfiguration(UserDetailsService userDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
     }
 
     @Bean
@@ -30,41 +35,25 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-// ------- This throws errors from IntelliJ - has to be discussed - here we need to use LAMBDA !- //
-*/
-/*    @Bean
-    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
-
-        http
-                .csrf().disable()
-                .httpBasic().disable()
-                .cors().and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/info").hasRole("USER")
-                .requestMatchers("/users/**").hasAnyRole("ADMIN", "USER")
-                .requestMatchers("/admins").hasAuthority("ROLE_ADMIN")
-                .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
-                .requestMatchers("/authenticate").permitAll()
-
-                .anyRequest().denyAll()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        return http.build();
-    }*//*
-
-
     @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("SELECT username, password, enabled" +
-                        " FROM users" +
-                        " WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT username, authority" +
-                        " FROM authorities " +
-                        " WHERE username=?");
-        return authenticationManagerBuilder.build();
+    protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
+// TODO : add HTTP requests according to what USER / Admin is authorized to do within the system //
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        // here request matchers for users ? //
+                        .requestMatchers("/televisions").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/televisions").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
+
+
+}
 }
 
 
-*/
