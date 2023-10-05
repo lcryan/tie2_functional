6d9427de1c69fbe6faecf1cfdcd7d42d2cad4c03
LuciaRyan <lcryan06@gmail.com>
2023-10-05 21:01:50 +0200

    added some code - but need to build different classes still - on hold for now

--- src/main/java/com/example/tie2/utils/JwtUtil.java
+++ src/main/java/com/example/tie2/utils/JwtUtil.java
@@ -1,4 +1,12 @@
 package com.example.tie2.utils;
 
 public class JwtUtil {
+    private final static String SECRET_KEY = "secret key has to be filled in here app."; //TODO : secret key has to be set up //
+
+    public String extractUsername(String token) {
+        return null; // TODO has to be filled in with a method ? //
+    }
 }
+
+
+//TODO : not complete yet : has to be completed after setting up classes etc. //
