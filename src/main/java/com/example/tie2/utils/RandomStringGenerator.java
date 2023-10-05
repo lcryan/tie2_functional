424d114e34ca266daba12adbf6b58f4b06511a58
LuciaRyan <lcryan06@gmail.com>
2023-10-05 21:01:22 +0200

    set up Random String Generator

--- src/main/java/com/example/tie2/utils/RandomStringGenerator.java
+++ src/main/java/com/example/tie2/utils/RandomStringGenerator.java
@@ -1,4 +1,28 @@
 package com.example.tie2.utils;
 
+import java.util.Random;
+
 public class RandomStringGenerator {
+
+    public static String generateString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        System.out.println(generatedString);
+
+        return generatedString;
+    }
 }
+
+
+// String generator example derived from Baeldung as an example //
+// Method still has to be used// //wondering if the params should be empty ? //
