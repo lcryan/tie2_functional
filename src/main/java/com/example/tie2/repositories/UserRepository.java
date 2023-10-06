6c076a09720fe65caf7da6bf59077dbee083f9b8
LuciaRyan <lcryan06@gmail.com>
2023-10-06 11:29:03 +0200

    set up user rep

--- src/main/java/com/example/tie2/repositories/UserRepository.java
+++ src/main/java/com/example/tie2/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.tie2.repositories;
 
-public interface UserRepository {
+import com.example.tie2.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
