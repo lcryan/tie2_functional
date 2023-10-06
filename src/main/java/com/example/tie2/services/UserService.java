3d1a94ea28a67a95972e92cd441327c274acf130
LuciaRyan <lcryan06@gmail.com>
2023-10-06 14:23:25 +0200

    built some functions - still incomlplete and one error atm - in process

--- src/main/java/com/example/tie2/services/UserService.java
+++ src/main/java/com/example/tie2/services/UserService.java
@@ -1,12 +1,15 @@
 package com.example.tie2.services;
 
 import com.example.tie2.dtos.UserDto;
+import com.example.tie2.exceptions.UsernameNotFoundException;
 import com.example.tie2.models.User;
 import com.example.tie2.repositories.UserRepository;
+import com.example.tie2.utils.RandomStringGenerator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class UserService {
 
@@ -19,22 +22,90 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    // --- getting all existing users --- //
     public List<UserDto> getAllUsers() {
-        List<User> userList = userRepository.findAll();
         List<UserDto> userDtoList = new ArrayList<>();
+        List<User> userList = userRepository.findAll();
         for (User user : userList) {
-            /*UserDto userDto = // */
+            UserDto userDto = userToUserDto(user);
+            userDtoList.add(userDto);
+        }
+        return userDtoList;
+    }
+
+    // --- creating a new user --- //
+    public String createNewUser(UserDto userDto) {
+        String randomString = RandomStringGenerator.generateString(33);
+        userDto.setApikey(randomString);
+        User newUser = userRepository.save(userDtotoUser(userDto));
+        return newUser.getUsername();
+    }
+
+    // --- getting a user by username --- //
+    public UserDto getUser(String username) {
+        Optional<User> optionalUser = userRepository.findById(Long.valueOf(username)); // long.valueOf method converts username into a Long value - which we need for user id //TODO: check if functional //
+        if (optionalUser.isPresent()) {
+            return userToUserDto(optionalUser.get());
+        } else {
+            throw new UsernameNotFoundException(username);
         }
     }
 
-    // Transfer Methods for User - model - dto - input dto - model //
-// TODO : has to be finalized - after setting up UserDto and InputUserDto ! //
-    public UserDto userToUserDto(User user) {
-        UserDto userDto = new UserDto();
+    // --- checking, if user actually exists --- //
+    public boolean userExists(String username) {
+        return userRepository.existsById(Long.valueOf(username));
+    }
+
+    public void updateUser(String username, UserDto newUser) {
+        if (userRepository.existsById(Long.valueOf(username))) {
+            User user = userRepository.findById(Long.valueOf(username)).get();
+
+            user.setUsername(newUser.getUsername());
+            user.setPassword(newUser.getPassword());
+            user.setFirstname(newUser.getFirstname());
+            user.setLastname(newUser.getLastname());
+            user.setEmail(newUser.getEmail());
+
+            return userRepository.save();
+
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
 
     }
 
 
+    // TRANSFER METHODS for User - model - dto -  model --- decided to go without the input dto - as it is the same as the user dto //
+
+    public UserDto userToUserDto(User user) {
+
+        var userDto = new UserDto();
+
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setFirstname(user.getFirstname());
+        userDto.setLastname(user.getLastname());
+        userDto.setEmail(user.getEmail());
+        userDto.setEnabled(user.isEnabled());
+        userDto.setAuthoritySet(user.getAuthoritySet());
+        userDto.setApikey(user.getApikey());
+
+        return userDto;
+    }
+
+    public User userDtotoUser(UserDto userDto) {
+        var user = new User();
+
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setFirstname(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setEmail(userDto.getEmail());
+        user.setEnabled(userDto.getEnabled());
+        user.setApikey(userDto.getApikey());
+
+        return user;
+    }
 }
 
 
