3b67c1d892e541c1fb567e0ad539f4d813dee5be
LuciaRyan <lcryan06@gmail.com>
2023-10-06 13:54:49 +0200

    built standard username not found exception

--- src/main/java/com/example/tie2/exceptions/UsernameNotFoundException.java
+++ src/main/java/com/example/tie2/exceptions/UsernameNotFoundException.java
@@ -1,4 +1,12 @@
 package com.example.tie2.exceptions;
 
-public class UsernameNotFoundException {
+import java.io.Serial;
+
+public class UsernameNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public UsernameNotFoundException(String username) {
+        super("Cannot find user " + username);
+    }
 }
