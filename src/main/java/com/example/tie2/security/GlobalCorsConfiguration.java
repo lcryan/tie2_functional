62be178bec837a2351c5c684a13b54c70931805c
LuciaRyan <lcryan06@gmail.com>
2023-10-10 10:08:04 +0200

    methods show as deprecated - check on this

--- src/main/java/com/example/tie2/security/GlobalCorsConfiguration.java
+++ src/main/java/com/example/tie2/security/GlobalCorsConfiguration.java
@@ -1,4 +1,15 @@
 package com.example.tie2.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
 public class GlobalCorsConfiguration {
-}
+
+// TODO: check on this code : how to implement the right methods : known ones are deprecated //
+            }
+
