f9b6065b8e581ba4caa54130e5f6cf6073cef068
LuciaRyan <lcryan06@gmail.com>
2023-10-11 09:20:37 +0200

    standard code for corsconfig frontend inserted

--- src/main/java/com/example/tie2/security/GlobalCorsConfiguration.java
+++ src/main/java/com/example/tie2/security/GlobalCorsConfiguration.java
@@ -2,14 +2,26 @@ package com.example.tie2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// -- THIS IS FOR THE COMMUNICATION WITH THE FRONT-END - because the frontend runs on localhost:3000 and the backend on localhost:8080 -- not necessary, if you build a backend application only//
 
 @Configuration
 @EnableWebMvc
 public class GlobalCorsConfiguration {
-
-// TODO: check on this code : how to implement the right methods : known ones are deprecated //
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
             }
+        };
+    }
+}
+
 
